3d4976f7b0a4c4c7e5c2e62ecd01e51a462eaa62
isaiahboylston <109104818+isaiahboylston@users.noreply.github.com>
2022-12-19 22:36:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const header = document.querySelector("header");
+
+    window.addEventListener ("scroll", function() {
+        header.classList.toggle ("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick =() => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+};
+
+menu.onscroll =() => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('active');
+};
+
+const sr = ScrollReveal ({
+    distance: '45px',
+    duration: 2700,
+    reset: true
+})
+
+sr.reveal('.home-text',{delay:350, orgin:'left'})
+sr.reveal('.home-text',{delay:350, orgin:'right'})
+
+sr.reveal('.sub-service,.about,portfolio,.service,.cta,.contact',{delay:200, orgin:'right'})
